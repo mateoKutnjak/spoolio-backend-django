cf4134ec6f253420d3af18665b20519156c5740e
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-11-18 20:02:49 +0100

    cors changed to 3000

--- spoolio_backend/settings.py
+++ spoolio_backend/settings.py
@@ -136,8 +136,8 @@ REST_AUTH_SERIALIZERS = {
 }
 
 CORS_ORIGIN_WHITELIST = (
-        'http://localhost:8080',
-        'http://127.0.0.1:8080',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
 )
 
 # Internationalization
