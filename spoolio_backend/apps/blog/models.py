ee09735609a53577e71c0692c9b189fe0ce14fd7
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-11-18 18:43:10 +0100

    Models update

--- spoolio_backend/apps/blog/models.py
+++ spoolio_backend/apps/blog/models.py
@@ -6,10 +6,22 @@ from ...libs import models as common_models
 
 class Blog(common_models.SoftDeleteModel):
 
+    BLOG_CATEGORY_CHOICES = (
+        ('3d-printing', '3D Printing'),
+        ('electronics', 'Electronics'),
+        ('design', 'Design'),
+    )
+
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     title = models.CharField(max_length=256)
+    subtitle = models.CharField(max_length=1024)
+
     content = models.TextField()
+
+    picture = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     
+    type = models.CharField(max_length=16, choices=BLOG_CATEGORY_CHOICES)
+
     def __str__(self):
         return "{}: {}".format(self.author.username, self.title)
--- spoolio_backend/apps/store/models.py
+++ spoolio_backend/apps/store/models.py
@@ -9,6 +9,8 @@ class Product(common_models.SoftDeleteModel):
     title = models.CharField(max_length=256)
     description = models.TextField()
 
+    picture = models.ImageField(upload_to='product_images/', null=True, blank=True)
+
     price = models.DecimalField(max_digits=24, decimal_places=2)
     
     def __str__(self):
