e7eb867ce507a70dd9b95d93f76c20b7bd3a1b9d
mateoKutnjak <mateo.kutnjak7@gmail.com>
2022-12-23 19:01:09 +0100

    Tmp

--- spoolio_backend/apps/user_profile/serializers.py
+++ spoolio_backend/apps/user_profile/serializers.py
@@ -5,7 +5,17 @@ from rest_framework import serializers
 from . import models
 
 
+class AddressSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Address
+        fields = '__all__'
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
+
+    shipping_address = AddressSerializer(read_only=True)
+    billing_address = AddressSerializer(read_only=True)
 
     class Meta:
         model = models.UserProfile
