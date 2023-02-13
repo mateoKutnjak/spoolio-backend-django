bf861a0130edb90e6c66764e141c54bcbaf5fe79
mateoKutnjak <mateo.kutnjak7@gmail.com>
2023-02-13 21:16:15 +0100

    Google sign up (localhost)

--- spoolio_backend/apps/authentication/urls.py
+++ spoolio_backend/apps/authentication/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 
+from . import views
+
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
+
+    path('accounts/', include('allauth.urls')),
+    
+    path('registration/google/', views.GoogleLogin.as_view(), name='google_login'),
 ]
--- spoolio_backend/apps/authentication/views.py
+++ spoolio_backend/apps/authentication/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 
-# Create your views here.
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+from allauth.socialaccount.providers.oauth2.views import (
+    OAuth2CallbackView,
+    OAuth2LoginView,
+)
+
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+
+class GoogleOAuth2AdapterIdToken(GoogleOAuth2Adapter):
+    def complete_login(self, request, app, token, **kwargs):
+        idinfo = id_token.verify_oauth2_token(token.token, requests.Request(), app.client_id)
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+        idinfo['id'] = idinfo['sub']
+        extra_data = idinfo
+        login = self.get_provider() \
+            .sociallogin_from_response(request,
+                                       extra_data)
+        return login
+
+oauth2_login = OAuth2LoginView.adapter_view(GoogleOAuth2AdapterIdToken)
+oauth2_callback = OAuth2CallbackView.adapter_view(GoogleOAuth2AdapterIdToken)
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2AdapterIdToken # ! This class used as a FIX (see https://gonzafirewall.medium.com/google-oauth2-and-django-rest-auth-92b0d8f70575)
+    client_class = OAuth2Client
