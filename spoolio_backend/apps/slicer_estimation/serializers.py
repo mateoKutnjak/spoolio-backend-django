3358b422a083286ed901e52ff32ee97c959f3cc1
mateoKutnjak <mateo.kutnjak7@gmail.com>
2023-04-05 23:25:12 +0200

    Add local_url identificator to prusa slicer serializer

--- spoolio_backend/apps/slicer_estimation/serializers.py
+++ spoolio_backend/apps/slicer_estimation/serializers.py
@@ -6,3 +6,5 @@ from .. print_order import models as print_order_models, serializers as print_or
 class PrintOrderUnitSlicerEstimationSerializer(print_order_serializers.PrintOrderUnitSerializer):
 
     order = serializers.PrimaryKeyRelatedField(queryset=print_order_models.PrintOrder.objects.all(), required=False)
+
+    local_url = serializers.CharField(required=True, help_text="Used as ID to identify the print order unit od response to frontend")
