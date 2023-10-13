f40b6c833eecfb11d8b0de7e884a512ea5235cfa
Bojan Spahija <bojan.spahija@gmail.com>
2023-10-13 17:41:12 +0200

    Init Payment

--- spoolio_backend/apps/payments/urls.py
+++ spoolio_backend/apps/payments/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('create-payment-intent/', views.create_payment),
+    path('init-payment-intent/', views.init_payment),
     path('stripe-webhooks/', views.stripe_webhooks)
 ]
--- spoolio_backend/apps/payments/views.py
+++ spoolio_backend/apps/payments/views.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 stripe.api_key = settings.STRIPE_API_KEY
 
 # * WHen checking price this is allowed difference (in cents)
-EPSILON = 3 
+EPSILON = 3
 
 
 def check_print_order_amount(print_order_id: int, amount: float):
@@ -31,12 +31,13 @@ def check_print_order_amount(print_order_id: int, amount: float):
     try:
         obj = print_order_models.PrintOrder.objects.get(pk=print_order_id)
 
-        total_price = float(obj.estimated_price) + float(obj.shipping_method.price)
+        total_price = float(obj.estimated_price) + \
+            float(obj.shipping_method.price)
 
-        # ! If rounding of number changes (here we use math.floor), make 
+        # ! If rounding of number changes (here we use math.floor), make
         # ! sure same method is used on frontend price calculation
-        total_price = math.floor(total_price * 100) / 100 
-    
+        total_price = math.floor(total_price * 100) / 100
+
         # * Stripe amount is measured x100
         stripe_amount = int(total_price * 100)
 
@@ -45,19 +46,20 @@ def check_print_order_amount(print_order_id: int, amount: float):
         return False, None, 'Estimated price and amount requested does not match'
     except ObjectDoesNotExist:
         return False, None, 'Print order with this id does not exist'
-    
+
 
 def check_modeling_order_amount(modeling_order_id: int, amount: float):
     try:
-        obj = modeling_order_models.ModelingOrder.objects.get(pk=modeling_order_id)
+        obj = modeling_order_models.ModelingOrder.objects.get(
+            pk=modeling_order_id)
 
         # ! Save tax percentage as constant or depending on country
         total_price = float(obj.estimated_price) * 1.25
 
-        # ! If rounding of number changes (here we use math.floor), make 
+        # ! If rounding of number changes (here we use math.floor), make
         # ! sure same method is used on frontend price calculation
-        total_price = math.floor(total_price * 100) / 100 
-    
+        total_price = math.floor(total_price * 100) / 100
+
         # * Stripe amount is measured x100
         stripe_amount = int(total_price * 100)
 
@@ -72,15 +74,16 @@ def check_store_order_amount(store_order_id: int, amount: float):
     try:
         obj = store_order_models.StoreOrder.objects.get(pk=store_order_id)
 
-        items_price = obj.items.aggregate(total_price=Sum(F('storeorderunit__quantity') * F('price')))['total_price']
+        items_price = obj.items.aggregate(total_price=Sum(
+            F('storeorderunit__quantity') * F('price')))['total_price']
 
         # ! Save tax percentage as constant or depending on country
         total_price = items_price * 1.25 + float(obj.shipping_method.price)
 
-        # ! If rounding of number changes (here we use math.floor), make 
+        # ! If rounding of number changes (here we use math.floor), make
         # ! sure same method is used on frontend price calculation
-        total_price = math.floor(total_price * 100) / 100 
-    
+        total_price = math.floor(total_price * 100) / 100
+
         # * Stripe amount is measured x100
         stripe_amount = int(total_price * 100)
 
@@ -117,12 +120,14 @@ def update_order_status(order_id: int, order_type: str):
         except ObjectDoesNotExist:
             order_not_found = True
     else:
-        raise Exception('Cannot update order [id={}] status. Order type {} not recognized.'.format(order_id, order_type))
+        raise Exception('Cannot update order [id={}] status. Order type {} not recognized.'.format(
+            order_id, order_type))
 
     if order_not_found:
         send_mail(
             'Manual intervention needed',
-            'Trying to update {} order no.{} status as payment has been just made, but this order has not been found in DB'.format(order_type, order_id),
+            'Trying to update {} order no.{} status as payment has been just made, but this order has not been found in DB'.format(
+                order_type, order_id),
             'bot@spoolio.net',
             ['spoolio.web@gmail.com'],
             fail_silently=False,
@@ -141,7 +146,8 @@ def create_payment(request):
     elif request.data['service'] == 'store':
         check_function = check_store_order_amount
 
-    is_amount_ok, amount, message = check_function(request.data['id'], request.data['amount'])
+    is_amount_ok, amount, message = check_function(
+        request.data['id'], request.data['amount'])
 
     if not is_amount_ok:
         return JsonResponse(data={'error': message}, status=400)
@@ -172,12 +178,80 @@ def create_payment(request):
 
 
 @api_view(['POST'])
+def init_payment(request):
+
+    try:
+        # Create a PaymentIntent with the order amount and currency
+
+        intent = stripe.PaymentIntent.create(
+            amount=50,
+            currency=request.data['currency'],
+            automatic_payment_methods={
+                'enabled': False,
+            },
+            capture_method="manual",
+            metadata={
+                'service': request.data['service'],
+            }
+            # payment_method_types=['card']
+        )
+
+        return JsonResponse(data={
+            'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        print(e)
+        return JsonResponse(data={'error': str(e)}, status=403)
+
+
+@api_view(['POST'])
+def modify_payment(request):
+
+    try:
+        # Modify a PaymentIntent's amount, customer and metadata -> order_id
+
+        client_secret = request.data['client_secret']
+        order_id = request.data['order_id']
+
+        try:
+            obj = print_order_models.PrintOrder.objects.get(pk=order_id)
+
+            total_price = float(obj.estimated_price) + \
+                float(obj.shipping_method.price)
+
+            # ! If rounding of number changes (here we use math.floor), make
+            # ! sure same method is used on frontend price calculation
+            total_price = math.floor(total_price * 100) / 100
+
+            # * Stripe amount is measured x100
+            stripe_amount = int(total_price * 100)
+
+        except ObjectDoesNotExist:
+            return JsonResponse(data={
+                'error': 'Print order with this id does not exist'
+            }, status=403)
+
+        intent = stripe.PaymentIntent.modify(
+            client_secret,
+            amount=stripe_amount,
+            metadata={"order_id": order_id},
+        )
+
+        return JsonResponse(data={
+            'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        print(e)
+        return JsonResponse(data={'error': str(e)}, status=403)
+
+
+@api_view(['POST'])
 def stripe_webhooks(request):
 
     event = request.data
-    
-    # ? See https://stripe.com/docs/webhooks/quickstart 
-    # ? if endpoint_secret is defined 
+
+    # ? See https://stripe.com/docs/webhooks/quickstart
+    # ? if endpoint_secret is defined
 
     logger.info('Stripe webhook of type {} received'.format(event['type']))
 
